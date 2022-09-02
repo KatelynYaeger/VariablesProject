b6a9fe46c655eeafd7833c4210eff79f43353a45
Katelyn Yaeger <knyaeger93@gmail.com>
2022-09-01 20:52:33 -0500

    initial commit

---
+++ VariablesProject1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace VariablesProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            string dogName = "Puddles";
+            int dogAge = 2;
+            char nickName = 'P';
+            bool hatesMeat = true;
+            double tennisBalls = 45;
+            decimal weight = 82.3m;
+
+            Console.WriteLine($"My dog's name is {dogName}. Her nickname is {nickName}. She is {dogAge} years old and weighs {weight} pounds.It is {hatesMeat} that she hates meat and she has {tennisBalls} tennis balls");
+
+
+          
+        }
+    }
+}
+
